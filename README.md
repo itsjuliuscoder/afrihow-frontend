ffc98c0d7a260d6377bf0bab3a5dca4935291510
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2019-10-10 10:29:04 +0100

    Landing page design with angular, full design coming up shortly

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Index/Landing Page side. 
+
+I will be completing the design before friday evening, I just added it so that you guys can see what I am working on. 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './public/index/banner/banner.component';
+import { FeatureComponent } from './public/index/feature/feature.component';
+import { FooterComponent } from './public/index/footer/footer.component';
+import { ProductComponent } from './public/index/product/product.component';
+import { QuoteComponent } from './public/index/quote/quote.component';
+import { NavbarComponent } from './public/index/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    FeatureComponent,
+    FooterComponent,
+    ProductComponent,
+    QuoteComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
